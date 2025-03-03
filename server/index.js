868a39c230892e6eda346f36424bbf3231fa8dfd
dhirendravsingh <dhirendravsingh09@gmail.com>
2025-03-04 03:10:48 +0530

    second commit

--- server/index.js
+++ server/index.js
@@ -3,7 +3,7 @@ const {ApolloServer} = require("@apollo/server");
 const {expressMiddleware} = require("@apollo/server/express4");
 const bodyParser = require("body-parser");
 const cors = require("cors");
-
+const axios = require("axios");
 
 async function startServer(){
     const app = express();
